518b04a02bebe4a61a273fbb66f08413c547d31a
Stephen Sauceda <contact@stephensauceda.com>
2018-09-15 19:45:07 -0400

    hook up mdx

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import App, {Container} from 'next/app'
+import React from 'react'
+import { MDXProvider } from '@mdx-js/tag'
+
+import Code from '../components/Code'
+import Title from '../components/Title'
+import Paragraph from '../components/Paragraph'
+import Image from '../components/Image'
+import Link from '../components/Link'
+import InlineCode from '../components/InlineCode'
+
+class MyApp extends App {
+  render () {
+    const {Component, pageProps,} = this.props
+    return (
+      <Container>
+        <MDXProvider components={{
+          p: Paragraph,
+          h1: Title,
+          code: Code,
+          inlineCode: InlineCode,
+          img: Image,
+          a: Link
+        }}>
+          <Component {...pageProps} />
+        </MDXProvider>
+      </Container>
+    )
+  }
+}
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react'
 import Head from 'next/head'
+import Link from '../components/Link'
+import Paragraph from '../components/Paragraph'
 
 const Index = () => (
   <Fragment>
@@ -10,18 +12,18 @@ const Index = () => (
       <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
     </Head>
     <div className="h-card">
-      <p>
+      <Paragraph>
         <strong>I'm <span className="p-name">Stephen Sauceda</span>.</strong>
-      </p>
-      <p className="p-note">
+      </Paragraph>
+      <Paragraph className="p-note">
         I'm a dad, husband and builder of web things. Currently, I'm a <span className="p-job-title">Web Engineer</span> for
         Apartment Therapy Media where I work remotely on{' '}
-        <a href="https://www.apartmenttherapy.com">Apartment Therapy</a> and{' '}
-        <a href="https://www.thekitchn.com">Kitchn</a>.
-      </p>
-      <p>
-        I can be found around the internet on <a href="https://github.com/stephensauceda" rel="me" className="u-url">Github</a>, <a href="https://www.instagram.com/stephensauceda/" rel="me" className="u-url">Instagram</a> and <a href="https://twitter.com/stephensauceda" rel="me" className="u-url">Twitter</a> or in real life somewhere in <span className="p-region">South Carolina</span>.
-      </p>
+        <Link href="https://www.apartmenttherapy.com">Apartment Therapy</Link> and{' '}
+        <Link href="https://www.thekitchn.com">Kitchn</Link>.
+      </Paragraph>
+      <Paragraph>
+        I can be found around the internet on <Link href="https://github.com/stephensauceda" rel="me" className="u-url">Github</Link>, <Link href="https://www.instagram.com/stephensauceda/" rel="me" className="u-url">Instagram</Link> and <Link href="https://twitter.com/stephensauceda" rel="me" className="u-url">Twitter</Link> or in real life somewhere in <span className="p-region">South Carolina</span>.
+      </Paragraph>
     </div>
     <style jsx global>{`
       html,
@@ -41,19 +43,8 @@ const Index = () => (
     <style jsx>{`
       div {
         padding: 1em;
-      }
-      p {
         max-width: 500px;
-        width: 90%;
-        line-height: 1.4;
-      }
-
-      a {
-        color: #e67e22;
-      }
-
-      a:visited, a:hover {
-        color: #d35400;
+        width: 90%
       }
     `}</style>
   </Fragment>
