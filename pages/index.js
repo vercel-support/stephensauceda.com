ad52295c696d895c5c80664de8a29e8d97192981
Stephen Sauceda <contact@stephensauceda.com>
2018-09-15 09:29:51 -0400

    metadata/indiewebify

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,22 @@ import Head from 'next/head'
 const Index = () => (
   <Fragment>
     <Head>
+      <title>Stephen Sauceda</title>
+      <meta name="description" content="Builder of web things." />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
-    <div>
+    <div className="h-card">
       <p>
-        <strong>I'm Stephen Sauceda.</strong>
+        <strong>I'm <span className="p-name">Stephen Sauceda</span>.</strong>
       </p>
-      <p>
-        I'm a dad, husband and builder of web things. Currently, I'm a Web Engineer for
+      <p className="p-note">
+        I'm a dad, husband and builder of web things. Currently, I'm a <span className="p-job-title">Web Engineer</span> for
         Apartment Therapy Media where I work remotely on{' '}
         <a href="https://www.apartmenttherapy.com">Apartment Therapy</a> and{' '}
         <a href="https://www.thekitchn.com">Kitchn</a>.
       </p>
       <p>
-        I can be found around the internet on <a href="https://github.com/stephensauceda">Github</a>, <a href="https://www.instagram.com/stephensauceda/">Instagram</a> and <a href="https://twitter.com/stephensauceda">Twitter</a> or in real life somewhere in South Carolina.
+        I can be found around the internet on <a href="https://github.com/stephensauceda" rel="me" className="u-url">Github</a>, <a href="https://www.instagram.com/stephensauceda/" rel="me" className="u-url">Instagram</a> and <a href="https://twitter.com/stephensauceda" rel="me" className="u-url">Twitter</a> or in real life somewhere in <span className="p-region">South Carolina</span>.
       </p>
     </div>
     <style jsx global>{`
