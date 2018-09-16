9eb914d676baaf4df6d8806bd57ab97916daeefd
Stephen Sauceda <contact@stephensauceda.com>
2018-09-16 15:04:50 -0400

    add emoji support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-const withMDX = require('@zeit/next-mdx')()
+const emoji = require('remark-emoji')
+const withMDX = require('@zeit/next-mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    mdPlugins: [emoji]
+  }
+})
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx']
