ca0c5ea94f5bb998a8454d6bf3581a1b1133565a
Stephen Sauceda <contact@stephensauceda.com>
2019-06-24 23:08:50 -0400

    need to figure out this testing library

---
+++ components/__tests__/Paragraph.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Paragraph from '../Paragraph'
+import { render, cleanup } from '@testing-library/react'
+
+afterEach(cleanup)
+
+test('should render children', () => {
+  const { getByText } = render((
+    <Paragraph>My paragraph.</Paragraph>
+  ))
+
+  expect(getByText('My paragraph.')).not.toBeNull()
+});
