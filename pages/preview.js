0209ec124a32c5d157da82c8714841c28a514d59
Stephen Sauceda <contact@stephensauceda.com>
2019-06-26 23:42:31 -0400

    typo

--- pages/preview.js
+++ pages/preview.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'React'
+import React, { Component } from 'react'
 import Head from 'next/head'
 import Router, { withRouter } from 'next/router'
 import linkResolver from '../lib/linkResolver'
