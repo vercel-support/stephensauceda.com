49f353e29c0d6d1222e872967a5576b36d03d775
Stephen Sauceda <contact@stephensauceda.com>
2019-06-27 22:43:13 -0400

    ga script

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,24 @@ export default class MyDocument extends Document {
       <Fragment>
         <html>
           <Head>
+            {process.env.NODE_ENV === 'production' && (
+              <Fragment>
+                <script
+                  async
+                  src="https://www.googletagmanager.com/gtag/js?id=UA-15647519-1"
+                />
+                <script
+                  dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-15647519-1');`
+                  }}
+                />
+              </Fragment>
+            )}
+
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta httpEquiv="Content-Language" content="en" />
             <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
