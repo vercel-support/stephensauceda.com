797e678c0a5280bd1cf8d495f46560826b0a6f7a
Stephen Sauceda <contact@stephensauceda.com>
2019-07-01 22:59:47 -0400

    don't need the comments anymore

--- rss/index.js
+++ rss/index.js
@@ -19,13 +19,9 @@ module.exports = async (req, res) => {
     pageSize: 10
   })
 
-  // create new feed instance
   const newFeed = new RSS(baseFeed)
-  // convert posts to html
   const formattedPosts = posts.map(post => convertPostToHTML(post))
-  // add formatted posts to feed
   formattedPosts.forEach(p => newFeed.item(p))
-  // return feed.xml()
   res.setHeader('content-type', 'text/xml')
   res.send(newFeed.xml())
 }
