e792b0c92708f1d463d1619d22cb6786c2504283
Stephen Sauceda <contact@stephensauceda.com>
2019-07-01 20:27:19 -0400

    blueprint

--- rss/all.js
+++ rss/all.js
@@ -1,1 +1,16 @@
-module.exports = (req, res) => res.status(404).send('still working on this')
+const RSS = require('rss')
+const ms = require('ms')
+const baseFeed = require('./lib/feed')
+
+let cache = null
+
+module.exports = (req, res) => {
+  if (!cache || Date.now() - cache.lastUpdated > ms('2h')) {
+    // fetch posts
+    // map over posts and convert to html
+    // assign to feed
+    // add to cache
+    cache = 'fetch data and return'
+  }
+  res.send(cache)
+}
---
+++ rss/lib/feed.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'Stephen Sauceda',
+  description: 'web. tech. geek.',
+  feed_url: 'https://stephensauceda.com/rss'
+}
